7fe7f3844e46fb1d23690fc9473e886ecef1ad66
Will Thompson <will.f.thompson@gmail.com>
2020-08-20 11:40:20 +0200

    working models and relationships. Cocktails, doses and ingredients

---
+++ app/models/cocktail.rb
@@ -0,0 +1,6 @@
+class Cocktail < ApplicationRecord
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+end
