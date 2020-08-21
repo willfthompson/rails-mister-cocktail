f21ecefb3758f88c0da9c3894f4a2936e22ca3b9
Will Thompson <will.f.thompson@gmail.com>
2020-08-21 14:44:52 +0200

    created seed data

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,7 +14,6 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(params_format)
-
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,39 @@ ingredient["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
   puts "Added #{ingredient["strIngredient1"]}"
 end
+
+def make_dose(cocktail)
+  dose = Dose.new(description: "#{rand(1..5)}measure", ingredient_id: rand(1..100))
+  dose.cocktail = cocktail
+  dose.save!
+  dose = Dose.new(description: "#{rand(1..5)}measure", ingredient_id: rand(1..100))
+  dose.cocktail = cocktail
+  dose.save!
+end
+
+def make_cocktail(url, name)
+  file = URI.open(url)
+  cocktail = Cocktail.new(name: "#{name.capitalize}")
+  cocktail.photo.attach(io: file, filename: "#{name.downcase}.jpg", content_type: 'image/jpg')
+  cocktail.save!
+
+  make_dose(cocktail)
+  make_dose(cocktail)
+  puts "made #{cocktail.name}"
+end
+
+make_cocktail('https://res.cloudinary.com/dx9ybadbm/image/upload/v1598006907/cosmopolitan_k75xxk.jpg', "cosmopolitan")
+make_cocktail("https://res.cloudinary.com/dx9ybadbm/image/upload/v1598006907/margarita_kdz1rr.jpg", "margarita")
+make_cocktail('https://res.cloudinary.com/dx9ybadbm/image/upload/v1598006908/mojito_mxttyg.jpg', 'mojito')
+make_cocktail('https://res.cloudinary.com/dx9ybadbm/image/upload/v1598006908/old_fashioned_coudcj.jpg', 'old fashioned')
+make_cocktail('https://res.cloudinary.com/dx9ybadbm/image/upload/v1598006908/espresso_martini_iqwzjc.jpg', 'espresso martini')
+make_cocktail('https://res.cloudinary.com/dx9ybadbm/image/upload/v1598006910/martini_fse9c7.jpg', 'martini')
+
+
+
+
+
+
+
+
+
